b58dac49e8b75b04da36522f3d05efbe2e371e10
andradew <wilson00749@gmail.com>
2024-06-19 18:31:36 -0500

    Refactor repository

--- internal/repository/task.go
+++ internal/repository/task.go
@@ -1,20 +1,32 @@
 package repository
 
-import "github.com/andradew/go/tasks/internal/dtos"
+import (
+	"github.com/andradew/go/tasks/internal/dtos"
+)
 
 type Repository struct {
+	Storage map[int]dtos.Task
 }
 
 func NewRepository() *Repository {
-	return &Repository{}
-}
-
-func (r *Repository) GetAllTasks() ([]dtos.Task, error) {
-	return dtos.Tasks{
-		{
+	return &Repository{Storage: map[int]dtos.Task{
+		1: {
 			ID:      1,
 			Name:    "Task One",
 			Content: "Some content",
 		},
-	}, nil
+		2: {
+			ID:      2,
+			Name:    "Task Two",
+			Content: "Some content",
+		},
+	}}
+}
+
+func (r *Repository) GetAllTasks() ([]dtos.Task, error) {
+	tasks := make([]dtos.Task, 0)
+	for _, v := range r.Storage {
+		tasks = append(tasks, v)
+	}
+	return tasks, nil
 }
